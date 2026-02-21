1bbe16bf43f084b7306f163db09f15c6c0508229
Nisarg Patel <nisargdp48@gmail.com>
2026-02-21 12:08:06 +0530

    Implement MongoDB connection and update dependencies.

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -3,6 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const connectDB = require('./config/db');
+
+// 1. Connect to database
+connectDB();
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
